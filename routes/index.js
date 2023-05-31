971f67ea93e5daab30d40b12cedd08727ed755b9
ofemjohn <ofemjohn@gmail.com>
2023-05-31 14:47:52 +0100

    corrected my export files in the controller folder

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
+import FilesController from '../controllers/FilesController';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const express = require('express');
 
